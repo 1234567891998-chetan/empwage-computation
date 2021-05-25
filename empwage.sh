404b929bbcfe6284132dea5d651a6698d0cd2da0
1234567891998-chetan <chaitanganeshraop@gmail.com>
2021-05-25 10:44:03 +0530

    initial Commit

--- empwage.sh
+++ empwage.sh
@@ -1,10 +1,3 @@
 #!/bin/bash -x
 echo "Welcome to empwage computation"
-is present = 1
-random check = $((RANDOM%2))
-if [ $ispresent -eq $randomCheck ]
-then
-	echo "employee is present"
-else
-	echo "employee is absent"
-fi
+
